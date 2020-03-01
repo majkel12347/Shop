2577917a931c42656f606acd863acbb502203e5d
Marcin <marcinurban560@gmail.com>
2020-03-01 23:06:03 +0100

    add menu-item component with style

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./directory.styles.scss";
+
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sections: [
+        {
+          title: "Dietary Supplements",
+          imageUrl:
+            "https://preparaty-invexu.pl/images/banery/moc_monojonowej_czasteczki.jpg",
+          id: 1,
+          linkUrl: "shop/dietary"
+        },
+        {
+          title: "Cosmetics",
+          imageUrl:
+            "https://www.invexremedies.pl/theme/Default/assets/img/ga-bg-4.jpg",
+          id: 2,
+          linkUrl: "shop/cosmetics"
+        },
+        {
+          title: "Profesional Line",
+          imageUrl:
+            "https://live.staticflickr.com/130/411453602_49363adf71_z.jpg",
+          id: 3,
+          linkUrl: "shop/professional"
+        },
+        {
+          title: "Sport Line",
+          imageUrl:
+            "https://img3.stockfresh.com/files/l/lunamarina/m/42/946121_stock-photo-blue-tiles-swimming-pool-water-reflection-texture.jpg",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/sport"
+        },
+        {
+          title: "For Soil",
+          imageUrl:
+            "https://www.publicdomainpictures.net/pictures/180000/nahled/digging-in-the-dirt.jpg",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/soil"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./homepage.styles.scss";
 
+import Directory from "../../components/directory/directory.component";
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
+      <Directory />
     </div>
   </div>
 );
